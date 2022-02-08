6a359afa1744ed67e1395bd2ad6140525b02a624
Wolfe Weeks <wwwkf9@umsystem.edu>
2022-02-07 19:46:01 -0600

    get opt functionality

--- simpleChain.c
+++ simpleChain.c
@@ -2,21 +2,56 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h> //for pid_t type
+#include <stdio.h>
+#include <getopt.h> //for getopt variables (e.g. opterr, optopt, etc.)
 
 int main(int argc, char *argv[])
 {
-  pid_t childpid = 0;
-  int i, n;
-  if (argc != 2)
+  int options = 0;
+
+  while (options != -1)
   {
-    fprintf(stderr, "Usage: %s processes\n", argv[0]);
-    return 1;
-  }
-  n = atoi(argv[1]);
-  for (i = 1; i < n; i++)
-    if (childpid = fork())
+    options = getopt(argc, argv, "hp:c:s:i:");
+    switch (options)
+    {
+    case 'h':
+      printf("usage: chain [-h] [-p nprocs] [-c nchars] [-s sleeptime] [-i niters] < textfile.txt\n");
       break;
-  fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
-          i, (long)getpid(), (long)getppid(), (long)childpid);
+    case 'p':
+      printf("-p");
+      break;
+    case 'c':
+      printf("-c");
+      break;
+    case 's':
+      printf("-s");
+      break;
+    case 'i':
+      printf("-i");
+      break;
+    case ':':
+      printf(":");
+      break;
+    case '?':
+      printf("unknown option: %c\n", optopt);
+      break;
+    default:
+      break;
+    }
+  }
+
+  // pid_t childpid = 0;
+  // int i, n;
+  // if (argc != 2)
+  // {
+  //   fprintf(stderr, "Usage: %s processes\n", argv[0]);
+  //   return 1;
+  // }
+  // n = atoi(argv[1]);
+  // for (i = 1; i < n; i++)
+  //   if (childpid = fork())
+  //     break;
+  // fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
+  //         i, (long)getpid(), (long)getppid(), (long)childpid);
   return 0;
 }
