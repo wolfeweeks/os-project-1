417466e2979b4d8b61cb78b3b736190bae6a558f
Wolfe Weeks <wwwkf9@umsystem.edu>
2022-02-08 10:07:58 -0600

    finished getopt functionality and added sleep and wait loops

--- simpleChain.c
+++ simpleChain.c
@@ -2,11 +2,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h> //for pid_t type
-#include <stdio.h>
+#include <sys/wait.h>
+// #include <stdio.h>
 #include <getopt.h> //for getopt variables (e.g. opterr, optopt, etc.)
 
 int main(int argc, char *argv[])
 {
+  if (argc < 2)
+  {
+    fprintf(stderr, "You must use options. Try running chain -h < textfile.txt for help\n");
+    return 1;
+  }
+
+  int numOfProcs = 4;
+  int numOfChars = 80;
+  int sleepTime = 3;
+  int numOfIterations = 1;
+
   int options = 0;
 
   while (options != -1)
@@ -16,42 +28,47 @@ int main(int argc, char *argv[])
     {
     case 'h':
       printf("usage: chain [-h] [-p nprocs] [-c nchars] [-s sleeptime] [-i niters] < textfile.txt\n");
-      break;
+      return 0;
     case 'p':
-      printf("-p");
+      numOfProcs = atoi(optarg);
       break;
     case 'c':
-      printf("-c");
+      numOfChars = atoi(optarg);
       break;
     case 's':
-      printf("-s");
+      sleepTime = atoi(optarg);
       break;
     case 'i':
-      printf("-i");
+      numOfIterations = atoi(optarg);
       break;
     case ':':
-      printf(":");
-      break;
+      printf("Option needs a value\n");
+      return 1;
     case '?':
-      printf("unknown option: %c\n", optopt);
-      break;
+      printf("%c is not an option\n", optopt);
+      return 1;
     default:
       break;
     }
   }
 
-  // pid_t childpid = 0;
-  // int i, n;
-  // if (argc != 2)
-  // {
-  //   fprintf(stderr, "Usage: %s processes\n", argv[0]);
-  //   return 1;
-  // }
-  // n = atoi(argv[1]);
-  // for (i = 1; i < n; i++)
-  //   if (childpid = fork())
-  //     break;
-  // fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
-  //         i, (long)getpid(), (long)getppid(), (long)childpid);
+  pid_t childpid = 0;
+  int i;
+  for (i = 1; i < numOfProcs; i++)
+  {
+    if (childpid = fork())
+    {
+      break;
+    }
+  }
+
+  int j;
+  for (j = 0; j < numOfIterations; j++)
+  {
+    sleep(3);
+    wait(NULL);
+    fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
+            i, (long)getpid(), (long)getppid(), (long)childpid);
+  }
   return 0;
 }
